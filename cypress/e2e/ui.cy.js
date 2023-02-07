03403d42fc123a566018b1d42a704a59124c5b71
moshiurr_ <mrahman9@upei.ca>
2023-02-06 23:14:16 -0400

    add few more ui tests

---
+++ cypress/e2e/ui.cy.js
@@ -0,0 +1,26 @@
+describe('UI tests', () => {
+    beforeEach( () => {
+        cy.visit("/wp-login.php");
+        cy.get( '#user_login' ).type( Cypress.env('username'));
+        cy.get( '#user_pass' ).type( Cypress.env('password') );
+        cy.get( '#wp-submit' ).click();
+    } );
+
+    it( 'click on avatar and go to profile page', () => {
+        cy.get('img.avatar.avatar-26.photo').click()
+        cy.url().should('contain', '/wp-admin/profile.php');
+    } );
+
+    it('should take us to network dashboard', () => {
+        cy.get('#wp-admin-bar-pb-network-admin > [aria-haspopup="true"]').click();
+        cy.url().should('contain', '/wp-admin/network/');
+    })
+
+    it('should take us to network plugin page', () =>{
+        cy.get('#wp-admin-bar-pb-network-admin > [aria-haspopup="true"]').click();
+        cy.get('#menu-plugins > .wp-has-submenu > .wp-menu-name').click();
+
+        // Pressbook plugin should be active
+        cy.get('tr.active > .plugin-title > strong').contains('Pressbooks');
+    })
+});
