4bcc9e44b0fb8fe0a3d5cd48217c6da972afdf9d
moshiurr_ <mrahman9@upei.ca>
2023-02-06 22:27:47 -0400

    add env variables for cypress, add run script to run cypress test headless

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    baseUrl: 'http://localhost/animal-behavior',
+    username: 'alif',
+    password: '1234'
+  },
   e2e: {
     baseUrl: 'http://localhost/animal-behavior',
     setupNodeEvents(on, config) {
